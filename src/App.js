f34aac4eddaf70c0e9ce402dd5ac0d93d55ee969
Ujjwol Bhandari <ubhandari2@pc-uzol.attlocal.net>
2019-07-28 17:22:47 -0500

    initial commit:

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I was changed!! 
+          To get started: Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
